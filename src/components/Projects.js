ecf4d0cc27243cf2d198d720b7aad43fcc1fa1b6
Alexis Rochefort <alexis.rochefort35@gmail.com>
2023-12-28 13:28:07 +0100

    Style projet + modif style liens avec bordure

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,46 @@
 import "../assets/styles/projects.scss";
 
-import logo from "../assets/images/projects/logo192.png";
+import { projectList } from "../datas/projectList";
 
 export default function Projects() {
     return (
         <div className="portfolio-projects" id="projects">
             <h1>Projets</h1>
             <div className="projects">
-                <div className="project">
+                {projectList.map((project, index) => {
+                    return (
+                        <div className="project" key={index}>
+                            <div className="project-head">
+                                <img
+                                    src={project.logo}
+                                    alt={project.name}
+                                ></img>
+                                <h3>{project.name}</h3>
+                            </div>
+                            <div className="project-body">
+                                <img
+                                    src={project.image}
+                                    alt={project.name}
+                                ></img>
+                                <p>{project.description}</p>
+                                <a
+                                    href={project.link}
+                                    rel="noreferrer"
+                                    target="_blank"
+                                >
+                                    Voir le projet
+                                </a>
+                            </div>
+                        </div>
+                    );
+                })}
+                {/* <div className="project">
                     <div className="project-head">
                         <img src={logo} alt="logo portfolio"></img>
                         <h3>Portfolio</h3>
                     </div>
                     <div className="project-body"></div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
---
+++ src/datas/projectList.js
@@ -0,0 +1,13 @@
+import logoPortfolio from "../assets/images/projects/logo192.png";
+import imagePortfolio from "../assets/images/projects/portfolio.png";
+
+export const projectList = [
+    {
+        name: "Portfolio",
+        logo: logoPortfolio,
+        image: imagePortfolio,
+        description:
+            "Ceci est mon portfolio où je présente mes compétences ainsi que mes projets. Je l'ai réalisé avec ReactJS.",
+        link: "https://github.com/NorlaXx/portfolio",
+    },
+];
