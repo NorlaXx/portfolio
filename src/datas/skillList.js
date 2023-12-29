08fd4adede957592dea73cbddee7609c8221220f
Alexis Rochefort <alexis.rochefort35@gmail.com>
2023-12-29 20:48:45 +0100

    Ajout logos compétences

--- src/datas/skillList.js
+++ src/datas/skillList.js
@@ -1,5 +1,6 @@
 import bootstrap from "../assets/images/skills/bootstrap.svg";
 import css from "../assets/images/skills/css3.svg";
+import sass from "../assets/images/skills/sass.svg";
 import git from "../assets/images/skills/git.svg";
 import bash from "../assets/images/skills/gnubash.svg";
 import html from "../assets/images/skills/html5.svg";
@@ -9,6 +10,8 @@ import php from "../assets/images/skills/php.svg";
 import react from "../assets/images/skills/react.svg";
 import symfony from "../assets/images/skills/symfony.svg";
 import uml from "../assets/images/skills/uml.svg";
+import java from "../assets/images/skills/java.svg";
+import sql from "../assets/images/skills/sql.svg";
 import front from "../assets/images/skills/front-end.svg";
 import back from "../assets/images/skills/back-end.svg";
 import devOps from "../assets/images/skills/dev-ops.svg";
@@ -25,6 +28,10 @@ export const skillList = [
             {
                 name: "Css",
                 image: css,
+            },
+            {
+                name: "Sass",
+                image: sass,
             },
             {
                 name: "Bootstrap",
@@ -45,6 +52,10 @@ export const skillList = [
         image: back,
         skills: [
             {
+                name: "Sql",
+                image: sql,
+            },
+            {
                 name: "Uml",
                 image: uml,
             },
@@ -59,6 +70,10 @@ export const skillList = [
             {
                 name: "Laravel",
                 image: laravel,
+            },
+            {
+                name: "Java",
+                image: java,
             },
         ],
     },
