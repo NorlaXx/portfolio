dd003f55d8e8033f7d554ad23e4b6d28c36c9dd2
alexis-rochefort <alexis.rochefort35@gmail.com>
2024-09-14 11:48:49 +0200

    ✨Ajout du projet ENI-BDE

--- src/datas/projectList.js
+++ src/datas/projectList.js
@@ -2,6 +2,8 @@ import logoPortfolio from "../assets/images/projects/logo-portfolio.svg";
 import imagePortfolio from "../assets/images/projects/portfolio.png";
 import imageEncheres from "../assets/images/projects/imageEnchere.png";
 import logoEncheres from "../assets/images/projects/logoEnchere.png";
+import logobdEni from "../assets/images/projects/logo-bdEni.png";
+import imagebdEni from "../assets/images/projects/BDE-ENI.png";
 
 export const projectList = [
     {
@@ -18,4 +20,11 @@ export const projectList = [
         image: imageEncheres,
         description: "Projet de mise en ligne d'enchères réalisé avec Java Spring Boot."
     },
+    {
+        name: "ENI-BDE",
+        logo: logobdEni,
+        image: imagebdEni,
+        description: "Projet d'application web pour la gestion des sorties organisés dans chaque campus de l'ENI, réalisé avec Symfony.",
+        link: "https://github.com/NorlaXx/ENI-BDE"
+    }
 ];
